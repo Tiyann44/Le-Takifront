c7098ff60e50018907a30d345c834df1d0c85c89
kitch naomi <91596994+kthnao@users.noreply.github.com>
2024-10-14 15:29:22 +0200

    page co moche

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,24 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-connexion',
     templateUrl: './connexion.component.html',
     styleUrls: ['./connexion.component.scss']
 })
-export class ConnexionComponent {
-    email: string = '';
-    password: string = '';
+export class ConnexionComponent implements OnInit {
 
-    onSubmit() {
-        // Remplacez cette partie par votre logique d'authentification
-       /* console.log('Email:', this.email);
-        console.log('Mot de passe:', this.password);*/
+    constructor() { }
 
-        // Exemple de logique d'authentification
-        // this.authService.login(this.email, this.password).subscribe(response => {
-        //     // Gérer la réponse du serveur
-        // }, error => {
-        //     // Gérer l'erreur
-        // });
+    ngOnInit(): void {
+        // Par défaut, afficher l'onglet de Connexion
+        const defaultTab = document.getElementById("defaultOpen");
+        if (defaultTab) {
+            defaultTab.click();
+        }
     }
+
+    openTab(evt: any, tabName: string): void {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+
+        // Masquer toutes les tabcontent
+        for (i = 0; i < tabcontent.length; i++) {
+            (tabcontent[i] as HTMLElement).style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+
+        // Supprimer la classe active de tous les boutons
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Afficher le contenu de l'onglet sélectionné
+        const selectedTab = document.getElementById(tabName);
+        if (selectedTab) {
+            selectedTab.style.display = "block";
+        }
+
+        // Ajouter la classe active au bouton cliqué
+        evt.currentTarget.className += " active";
+    }
+
+    closeModal(): void {
+        var modal = document.getElementById("authModal");
+        if (modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    protected readonly event = event;
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core"
+import {AfterViewInit, Component, OnInit} from "@angular/core"
 import { Link } from "models/links.model"
+import {Router} from "@angular/router";
 
 @Component({
   selector: "epf-navbar",
@@ -7,10 +8,17 @@ import { Link } from "models/links.model"
   styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
+
   links: Link[] = []
 
-  constructor() {
+  constructor(private router: Router) { }
+
+  openConnexion() {
+    // Redirige vers la page de connexion
+    this.router.navigate(['/connexion']);
+  }
+  /*constructor() {
     this.links.push({ name: "Students", href: "students" })
     this.links.push({ name: "Majors", href: "majors" })
-  }
+  }*/
 }
